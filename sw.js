0b7d095dff3223073eebe3ac3f03fe544e924b89
ernesto8019 <ERNSTLONGHI@GMAIL.COM>
2026-03-24 14:04:56 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,19 @@
+const CACHE_NAME = 'paintcalc-v1';
+const ASSETS = [
+  './index.html',
+  './manifest.json'
+];
+
+// Installazione e salvataggio file base
 self.addEventListener('install', (e) => {
-  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
-navigator.serviceWorker.register('./sw.js');
+
+// Gestione delle richieste (per far funzionare l'app offline)
 self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
 });
